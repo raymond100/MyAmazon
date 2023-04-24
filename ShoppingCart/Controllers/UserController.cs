a9ca5a2b614e5f6b08e0480e3bcf1c7c188492ce
Win Win Htike <winwinhtike23@gmail.com>
2023-04-24 02:07:55 -0500

    Logout
    Logout

---
+++ ShoppingCart/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+       
+        public UserController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                // This needs to be a redirect so that the browser performs a new
+                // request and the identity for the user gets updated.
+                return RedirectToRoute("/");
+            }
+        }
+    }
+}
